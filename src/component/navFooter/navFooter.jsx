e0d31d80eae0141510efcef9f379a2aea6100ba5
zhangyan <1099694846@qq.com>
2020-05-29 17:59:59 +0800

    xg

--- src/component/navFooter/navFooter.jsx
+++ src/component/navFooter/navFooter.jsx
@@ -11,14 +11,63 @@ class NavFooter extends Component {
 	constructor () {
 		super()
 		this.state = {
-
+			selectedTab: 0,
 		}
 	}
+
+	// 类型校验
+	static propsType = {
+		navList: PropTypes.array.isRequired
+	}
+	
+	// 设置默认值
+	static defaultProps = {
+		navList: []
+	}
+
 	render () {
-
+		let { navList } = this.props
 		return (
-			<div>hello</div>
-
+			<div style={ {position:'fixed',bottom: 0, width: '100%'} }>
+		        <TabBar
+		          unselectedTintColor="#949494"
+		          tintColor="#33A3F4"
+		          barTintColor="white"
+		        >
+			       
+			        {
+			        	navList.map( (item,index) => {
+			        		return (
+			        			<TabBar.Item
+			        			    title={item.title}
+			        			    key={item.title}
+			        			    icon={<div style={{
+			        			    	width: '22px',
+			        			      	height: '22px',
+			        			      	background:`url(${require(`./images/${item.icon}.png`)}) center center /  21px 21px no-repeat` }}
+			        			    />}
+			        			    selectedIcon={<div style={{
+			        			      width: '22px',
+			        			      height: '22px',
+			        			      background: `url(${require(`./images/${item.icon}-selected.png`)}) center center /  21px 21px no-repeat` }}
+			        			    />}
+			        			    selected={this.state.selectedTab == index}
+			        			    badge={10}
+			        			    onPress={() => {
+			        			      	this.setState({
+			        			        	selectedTab: index,
+			        			      	});
+			        			    }}
+			        			    data-seed="logId"
+			        			  >
+			        			    {/*{this.renderContent('Life')}fsfdsf*/}
+			        			</TabBar.Item>
+			        		)
+			        	})
+			        }
+			       
+		        </TabBar>
+			</div>
 		)
 	}
 }
